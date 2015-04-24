36038dd25777b79442f69c13af103ab39520157c
Patrick Rummage <patrickbrummage@gmail.com>
2015-04-24 03:14:22 -0400

    add uses all letters exercise

---
+++ ch9/uses_all.py
@@ -0,0 +1,31 @@
+'''
+uses_all.py
+
+Author: Patrick Rummage
+        [patrickbrummage@gmail.com]
+
+Objective:
+    Create a program that reads through a word list and prints out
+    all words which contain all letters in given string. The word file
+    and the 'required' string are given as commandline arguments.
+'''
+import sys
+
+word_list = open(sys.argv[1])
+
+required = sys.argv[2]
+
+def uses_all(word, req):
+    for ch in req:
+        if ch not in word:
+            return False
+    return True
+
+def use_all_filter(wf, req):
+    for line in wf:
+        word = line.strip()
+        if uses_all(word, req):
+            print word
+
+use_all_filter(word_list, required)
+word_list.close()
